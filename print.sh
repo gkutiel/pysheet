19a82d682985a762f8b87833115e256e4d420a2e
Gilad Kutiel <gkutiel@amazon.com>
2020-10-31 08:25:26 +0200

    -

--- print.sh
+++ print.sh
@@ -1,4 +1,9 @@
 #!/usr/local/bin/fish
-python main.py
+if test (count $argv) -gt 0
+    set config $argv[1]
+else
+    set config 'config.json'
+end
+python main.py --config-json $config
 latexmk -pdf main.tex
 lp main.pdf

dbdf6366103f22754b838483d3b312503ea0e338
Gilad Kutiel <gkutiel@amazon.com>
2021-08-10 17:12:32 +0300

    v0.0.1

---
+++ yael.sh
@@ -0,0 +1,5 @@
+#!/usr/local/bin/fish
+gk-basic
+latexmk -pdf main.tex
+lp main.pdf
+
--- yoav.sh
+++ yoav.sh
@@ -1,8 +1,4 @@
 #!/usr/local/bin/fish
-gk-basic
-latexmk -pdf main.tex
-lp main.pdf
-
 gk-basic --add [100, 200] --sub [100, 200] --div [2, 20] --mul [2, 20]
 latexmk -pdf main.tex
 lp main.pdf

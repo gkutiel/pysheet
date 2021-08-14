637d05a2b0bc6df5481b3179a152c7181d481ee8
Gilad Kutiel <gkutiel@amazon.com>
2021-08-14 09:40:10 +0300

    yoav.sh

--- yoav.sh
+++ yoav.sh
@@ -2,3 +2,5 @@
 gk-equation-1 && latexmk -pdf main.tex && lp main.pdf
 gk-equation-2 && latexmk -pdf main.tex && lp main.pdf
 gk-equation-3 && latexmk -pdf main.tex && lp main.pdf
+
+gk-frac-1 && latexmk -pdf main.tex && lp main.pdf

5940974baa26f1f6a61ccb6c76cd15d1a97f4d74
Gilad Kutiel <gilad.kutiel@gmail.com>
2022-06-05 08:18:09 +0300

    tex

--- yael.sh
+++ yael.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/fish
-latexmk -c && gk-arithmetic --add [1,200] && latexmk -pdf main.tex && lp main.pdf
-latexmk -c && gk-equation-1 -a [1,1] -x [3,10] -b [1,2] && latexmk -pdf main.tex && lp main.pdf
-# latexmk -c && gk-clock && latexmk -pdf main.tex && lp main.pdf
+gk-arithmetic --add [1,200] 
+gk-equation-1 -a [1,1] -x [3,10] -b [1,2]
+gk-clock 
 # gk-fun-square -n 5 -op '+' && latexmk -pdf main.tex && lp main.pdf
 
 # latexmk -c && gk-word-problems --max-x 3  --max-a 3 --max-b 10 && latexmk -pdf main.tex && lp main.pdf
--- yoav.sh
+++ yoav.sh
@@ -1,11 +1,11 @@
 #!/usr/local/bin/fish
 
-gk-arithmetic --add [10,600] --sub [10,600] --mul [2,30] --div [2,30] && latexmk -pdf main.tex && lp main.pdf
-# gk-fun-square && latexmk -pdf main.tex && lp main.pdf
-# gk-equation-2 && latexmk -pdf main.tex && lp main.pdf
-# gk-frac-1 && latexmk -pdf main.tex && lp main.pdf
+gk-arithmetic --add [10,600] --sub [10,600] --mul [2,30] --div [2,30]
+gk-equation-1
+gk-equation-2
+gk-equation-3
+gk-clock 
 
-gk-equation-1 && latexmk -pdf main.tex && lp main.pdf
-# gk-equation-3 && latexmk -pdf main.tex && lp main.pdf
-# gk-clock && latexmk -pdf main.tex && lp main.pdf
 # gk-word-problems && latexmk -xelatex main.tex && lp main.pdf
+# gk-frac-1 && latexmk -pdf main.tex && lp main.pdf
+# gk-fun-square && latexmk -pdf main.tex && lp main.pdf

a4d68050fbf5bbd25d87492a7918309fc03870b1
Gilad Kutiel <gilad.kutiel@gmail.com>
2022-06-22 08:34:26 +0300

    tex

--- yael.sh
+++ yael.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/fish
 gk-arithmetic --add [10,200] --sub [1,30] --mul [2,11] --div [2,11] --out 'yael/arit.tex'
 gk-equation-1 -a [1,1] -x [3,10] -b [1,2] --out 'yael/eq1.tex'
-gk-clock  --out 'yael/clock.tex'
+gk-clock --out 'yael/clock.tex'
 gk-fun-square -n 5 -op '+' --out 'yael/squr.tex'
 
 latexmk -pdf --outdir=yael yael/*.tex

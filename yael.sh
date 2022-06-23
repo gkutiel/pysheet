f4d3c81051627c2b648d5c4ce5418a8a4920d5b4
Gilad Kutiel <gilad.kutiel@gmail.com>
2022-06-23 08:33:58 +0300

    tex

--- yael.sh
+++ yael.sh
@@ -1,5 +1,6 @@
 #!/usr/local/bin/fish
-gk-arithmetic --add [10,200] --sub [1,30] --mul [2,11] --div [2,11] --out 'yael/arit.tex'
+gk-arithmetic --add [10,200] --sub [1,30] --mul [2,11] --div [2,11] --out 'yael/arit1.tex'
+gk-arithmetic --add [10,200] --sub [1,30] --mul [2,11] --div [2,11] --out 'yael/arit2.tex'
 gk-equation-1 -a [1,1] -x [3,10] -b [1,2] --out 'yael/eq1.tex'
 gk-clock --out 'yael/clock.tex'
 gk-fun-square -n 5 -op '+' --out 'yael/squr.tex'
--- yoav.sh
+++ yoav.sh
@@ -1,6 +1,8 @@
 #!/usr/local/bin/fish
 
-gk-arithmetic --add [10,600] --sub [10,600] --mul [2,30] --div [2,30] --out yoav/arit.tex
+gk-arithmetic --add [10,600] --sub [10,600] --mul [2,30] --div [2,30] --out yoav/arit1.tex
+gk-arithmetic --add [10,600] --sub [10,600] --mul [2,30] --div [2,30] --out yoav/arit2.tex
+gk-arithmetic --add [1,200] 
 gk-equation-1 --out 'yoav/eq1.tex'
 gk-equation-2 --out 'yoav/eq2.tex'
 gk-equation-3 --out 'yoav/eq3.tex'

522e01016bdf2fbb135ee8fc017cac3a7db762d1
Gilad Kutiel <gilad.kutiel@gmail.com>
2023-01-20 18:06:56 +0200

    yael

--- yael_batch.sh
+++ yael_batch.sh
@@ -5,6 +5,7 @@ for i in (seq 1 9);
 	gk-sub -a 10 -b 100 --out yael/yael_sub_$i.tex
 	gk-mul -a 2 -b 6 --out yael/yael_mul_$i.tex
 	gk-div -a 2 -b 6 --out yael/yael_div_$i.tex
+	gk-frac-4 --max-lcm 6 --out yael/yael$i.tex
 end
 
 latexmk -pdf --outdir=yael yael/yael_*.tex
